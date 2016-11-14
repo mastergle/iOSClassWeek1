881f298414938aa13a94e2864d563319cc2920b3
Barry, Elh <mastergle2002@gmail.com>
2016-11-14 14:03:54 -0500

    Change UITextField keyboard type to numberPad.

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -27,11 +27,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.lowerBoundField.keyboardType = .numberPad
+        self.upperBoundField.keyboardType = .numberPad
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
